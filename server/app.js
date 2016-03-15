317c39bc91afdbad17ba7ad51995b263d86b8f90
jmcilhargey <jmcilhargey@gmail.com>
2016-03-15 16:40:41 -0700

    Added Angular routing using ui-router for home page and individual sport pages. Included sample JSON to test backend endpoint.

--- server/app.js
+++ server/app.js
@@ -3,10 +3,19 @@
 let express = require("express");
 let app = express();
 
+let mongoUtil = require("./mongoUtil");
+mongoUtil.connect();
+
 app.use(express.static(__dirname + "/../client"));
 
 app.get("/sports", (req, res) => {
-	res.json(["Cyling", "Weightlifting"]);
+	let sports = mongoUtil.sports();
+	sports.find({}).toArray((err, docs) => {
+		console.log(JSON.stringify(docs));
+		let sportNames = docs.map((sport) => sport.name);
+		res.json(sportNames);
+	});
+	
 });
 
 app.listen(8080, () => console.log("Listening on port 8080."));
