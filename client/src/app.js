ab6678f089c83740a9db0c196de0305ff9b4016e
jmcilhargey <jmcilhargey@gmail.com>
2016-03-15 22:08:51 -0700

    Made Angular route for individual sports using  and connected Express route to MongoDB query

--- client/src/app.js
+++ client/src/app.js
@@ -3,49 +3,34 @@ import "angular-ui-router";
 
 angular.module("olympics", ["ui.router"])
 
-	.config(($stateProvider, $urlRouterProvider) => {
-		$urlRouterProvider.otherwise("/sports")
+.config(($stateProvider, $urlRouterProvider) => {
+	$urlRouterProvider.otherwise("/sports")
 
-		$stateProvider
-			.state("sports", {
-				url: "/sports",
-				templateUrl: "sports/sports-nav.html",
-				resolve: {
-					sportsService: function($http) {
-						return $http.get("/sports");
-					}
-				},
-				controller: function(sportsService) {
-					this.sports = sportsService.data;
-				},
-				controllerAs: "sportsCtrl"
-			})
-			.state("sports.medals", {
-				url: "/:sportName",
-				templateUrl: "sports/sports-medals.html",
-				resolve: {
-					sportService: function($q) {
-						return $q((resolve, reject) => {
-
-							let sport = {
-								"name": "Cycling",
-								"goldMedals": [{
-									"division": "Men's Sprint",
-									"country": "UK",
-									"year": 2012
-								}, {
-									"division": "Women's Sprint",
-									"country": "Australia",
-									"year": 2012
-								}]
-							};
-							resolve({ data: sport });
-						})
-					}
-				},
-				controller: function(sportService) {
-					this.sport = sportService.data;
-				},
-				controllerAs: "sportCtrl"
-			})
+	$stateProvider
+		.state("sports", {
+			url: "/sports",
+			templateUrl: "sports/sports-nav.html",
+			resolve: {
+				sportsService: function($http) {
+					return $http.get("/sports");
+				}
+			},
+			controller: function(sportsService) {
+				this.sports = sportsService.data;
+			},
+			controllerAs: "sportsCtrl"
+		})
+		.state("sports.medals", {
+			url: "/:sportName",
+			templateUrl: "sports/sports-medals.html",
+			resolve: {
+				sportService: function($http, $stateParams) {
+					return $http.get(`/sports/${ $stateParams.sportName }`);
+				}
+			},
+			controller: function(sportService) {
+				this.sport = sportService.data;
+			},
+			controllerAs: "sportCtrl"
+		})
 	})
--- server/app.js
+++ server/app.js
@@ -15,6 +15,23 @@ app.get("/sports", (req, res) => {
 		let sportNames = docs.map((sport) => sport.name);
 		res.json(sportNames);
 	});
+});
+
+app.get("/sports/:name", (req, res) => {
+	let sportName = req.params.name;
+
+	let sports = mongoUtil.sports();
+	sports.find({name: sportName}).limit(1).next((err, doc) => {
+
+		if (err) {
+			res.sendStatus(400);
+		}
+
+		console.log("Sport doc: " + doc);
+		res.json(doc);
+	});
+
+
 	
 });
 
