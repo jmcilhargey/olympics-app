3cc805d585de0b3933ebbf8c09803a5d9a763351
jmcilhargey <jmcilhargey@gmail.com>
2016-08-31 14:11:29 -0700

    Added graph directive with d3 script to generate world map with medal counts by country (opt, make interactive)

--- client/src/app.js
+++ client/src/app.js
@@ -37,6 +37,10 @@ angular.module("olympics", ["ui.router"])
 			},
 			controllerAs: "sportsCtrl"
 		})
+		.state("sports.graph", {
+			url: "/graph",
+			template: "<world-graph></world-graph>"
+		})
 		.state("sports.medals", {
 			url: "/:sportName",
 			templateUrl: "sports/sports-medals.html",
@@ -71,12 +75,91 @@ angular.module("olympics", ["ui.router"])
 			},
 			controllerAs: "newMedalCtrl"
 		})
-		.state("sports.graph", {
-			url: "/graph",
-			templateUrl: "sports/graph-results.html",
-			controller: function($stateParams, $state, $http) {
-				
-			},
-			controllerAs: "graphCtrl"
-		})
 	})
+
+.directive("worldGraph", function() {
+
+	return {
+		restrict: "E",
+		link: function(scope, element, attrs) {
+
+			var width = 1000;
+			var height = 500;
+
+			var canvas = d3.select(element[0])
+				.append("canvas")
+				.attr("width", width)
+				.attr("height", height);
+			
+			var context = canvas.node().getContext("2d")
+
+			var projection = d3.geoEquirectangular()
+				.scale(200);
+
+			var path = d3.geoPath()
+				.projection(projection)
+				.context(context);
+
+			d3.json("../data/world-map-data.json", function(error, world) {
+				
+				if (error) { throw error; }
+
+				path(topojson.feature(world, world.objects.countries));
+				context.globalAlpha = 0.6;
+				context.lineWidth = 1;
+				context.lineJoin = "round";
+				context.stroke();
+				path(topojson.feature(world, world.objects.land));
+				context.fillStyle = "#bfc0c0";
+				context.fill();
+
+				var radius = d3.scaleSqrt().domain([0, 125]).range([0, 50]);				
+
+				createPoints(topojson.feature(world, world.objects.countries).features);
+
+				function createPoints(worldData) {
+
+					var points = d3.select("canvas").append("div");
+
+					d3.json("../data/world-medal-data.json", function(error, medalData) {
+
+						if (error) { throw error; }
+
+						points.selectAll("points.arc")
+							.data(worldData)
+							.enter()
+							.append("points")
+							.classed("arc", true)
+							.attr("x", function(d) { return path.centroid(d)[0]; })
+							.attr("y", function(d) { return path.centroid(d)[1]; })
+							.attr("radius", function(d) { return radius(medalData[d.id] ? medalData[d.id].total : 0) })
+							.attr("fillStyle", "#00E676")
+							.attr("fillText", function(d) { return medalData[d.id] && medalData[d.id].total > 1 ? medalData[d.id].total : ""; }, function(d) { return path.centroid(d)[0]; }, function(d) { return path.centroid(d)[1]; });
+
+						drawPoints(points);
+					});
+				}
+
+				function drawPoints(points) {
+					
+					var elements = d3.selectAll("points.arc");
+					console.log(elements)
+					elements.each(function(d) {
+
+						    var node = d3.select(this);
+							
+		   					context.beginPath();
+							context.arc(node.attr("x"), node.attr("y"), node.attr("radius"), 0, 2 * Math.PI);
+							context.fillStyle = node.attr("fillStyle");
+		    				context.fill();
+		    				context.font = "16px arial";
+							context.fillStyle = "#FF3D00";
+							context.fillText(node.attr("fillText"), node.attr("x") - 10, node.attr("y"))
+		    				context.stroke();
+		    				context.closePath();
+					});
+				}
+			});
+		}
+	};
+});
